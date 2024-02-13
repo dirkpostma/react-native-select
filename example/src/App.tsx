c61c1243ad625117efdfe82f2c1d2b127e964551
Dirk Postma <d.postma@alfen.com>
2024-02-13 18:31:51 +0100

    feat: initial Select component with support for generic items, multi select

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,20 +1,47 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 
-import { StyleSheet, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { Select } from 'react-native-select';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Select />
-    </View>
-  );
+interface Person {
+  id: number;
+  name: string;
 }
 
+const people: Person[] = [
+  { id: 1, name: 'Bob' },
+  { id: 2, name: 'Alice' },
+];
+
+export const App = () => {
+  const [selectedPeople, setSelectedPeople] = useState<Person[]>([]);
+
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.title}>Select people:</Text>
+        <Select
+          items={people}
+          keyExtractor={(person) => person.id}
+          labelExtractor={(person) => person.name}
+          onSelect={setSelectedPeople}
+        />
+
+        <View>
+          <Text>{JSON.stringify(selectedPeople, null, 2)}</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
 const styles = StyleSheet.create({
+  title: {
+    fontWeight: '600',
+  },
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 16,
   },
 });
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,68 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { Pressable } from 'react-native';
+import { Text } from 'react-native';
 
-export const Select = () => {
+interface Props<T> {
+  items: T[];
+  keyExtractor: (item: T) => string | number;
+  labelExtractor: (item: T) => string;
+  onSelect: (selectedItems: T[]) => void;
+}
+
+export const Select = <T,>({
+  items,
+  keyExtractor,
+  labelExtractor,
+  onSelect,
+}: Props<T>) => {
+  const [selectedItems, setSelectedItems] = useState<T[]>([]);
+
+  const toggleItem = (item: T) => {
+    const isSelected = selectedItems.some(
+      (selectedItem) => keyExtractor(selectedItem) === keyExtractor(item)
+    );
+
+    const newSelectedItems = isSelected
+      ? selectedItems.filter(
+          (selectedItem) => keyExtractor(selectedItem) !== keyExtractor(item)
+        )
+      : [...selectedItems, item];
+
+    setSelectedItems(newSelectedItems);
+    onSelect(newSelectedItems);
+  };
+
   return (
-    <View>
-      <Text>Placeholder for Select component</Text>
-    </View>
+    <>
+      {items.map((item) => (
+        <Pressable
+          key={keyExtractor(item)}
+          style={styles.item}
+          onPress={() => toggleItem(item)}
+        >
+          <Text>
+            {selectedItems.some(
+              (selectedItem) =>
+                keyExtractor(selectedItem) === keyExtractor(item)
+            )
+              ? '✅ '
+              : '🔲 '}
+
+            {labelExtractor(item)}
+          </Text>
+        </Pressable>
+      ))}
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 16,
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: 'lightgray',
+    marginBottom: 8,
+  },
+});
